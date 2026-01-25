9111da64921808006afe07997e5eda75d1227a38
NowOne <nowone.though@gmail.com>
2026-01-25 23:24:52 +0900

    update contact-thanks

--- page-contact-thanks.php
+++ page-contact-thanks.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<main class="l-main p-contact">
+<main class="l-main p-contact-thanks">
     <section class="l-content l-content--inline l-center c-form__thanks">
         <!-- <canvas id="bg-lines"></canvas> -->
         <h1 class="c-heading-2xl">Contact Complete</h1>
