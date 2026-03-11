4c7646b7190efcae2ca3e76f5866598787f4ab26
NowOne <nowone.though@gmail.com>
2026-03-11 23:51:05 +0900

    modify taxonomy title

--- taxonomy-creation_type.php
+++ taxonomy-creation_type.php
@@ -5,14 +5,29 @@
 	$type = ($term && isset($term->slug)) ? $term->slug : 'default';
 	?>
 
-<main class="l-main">
-  <section class="l-content l-content--inline">
-      <h1 class="c-heading-2xl">
-        <?php single_term_title(); ?> - Discography
-      </h1>
-	      <?php
-	      get_template_part(
-	        'template-parts/creation/archive/archive',
+	<main class="l-main">
+	  <section class="l-content l-content--inline">
+	      <h1 class="c-heading-2xl">
+	        <?php
+	        $label_map = [
+	          'music' => 'Music',
+	          'movie' => 'Movie',
+	          'artwork' => 'Artwork',
+	        ];
+	        $suffix_map = [
+	          'music' => 'Discography',
+	          'movie' => 'Videography',
+	          'artwork' => 'Gallery',
+	        ];
+	        $term_title = single_term_title('', false);
+	        $label = $label_map[$type] ?? $term_title;
+	        echo esc_html($label);
+	        $suffix = $suffix_map[$type] ?? 'Discography';
+	        ?> - <?php echo esc_html($suffix); ?>
+	      </h1>
+		      <?php
+		      get_template_part(
+		        'template-parts/creation/archive/archive',
 	        $type,
 	        ['type' => $type]
 	      );
